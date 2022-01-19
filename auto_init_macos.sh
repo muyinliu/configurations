9a78841ebc0a070ce40e1d6efdf9c40db21d20a4
muyinliu <muyinliu@gmail.com>
2022-01-20 07:54:00 +0800

    Auto install zsh-syntax-highlighting

--- auto_init_macos.sh
+++ auto_init_macos.sh
@@ -319,6 +319,7 @@ function install_oh_my_zsh () {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     # install plugin zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
 # FIXME: should compile with Xcode?
